bb3c5bf17e1d9c4710561bbc7ad6b1caa7f8aac0
devil ghost <devgho@outlook.com>
2024-12-03 17:38:39 +0800

    modified database user

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:fjf88888@localhost:3306/baimabackend"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://baimabackend:fjf88888@localhost:3306/baimabackend"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
